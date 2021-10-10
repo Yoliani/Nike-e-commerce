48fa28c511369a4bbe4c126b076f89f9639cedbe
Yoliani <edrayoca@outlook.es>
2021-10-10 09:00:40 -0500

    commit

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ def index():
     return render_template("index.html")
 
 
-if __name__ == "__main__":  # Makes sure this is the main process
 
+if __name__ == "__main__":  # Makes sure this is the main process
     app.run(  # Starts the site
         debug=True,
         host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
