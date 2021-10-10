b8ebd58e5010725fed70de74ed403678f36a55bf
Yoliani <edrayoca@outlook.es>
2021-10-10 10:40:59 -0500

    commit

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import random
 
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
+from werkzeug.utils import redirect
 from wtforms.fields.core import StringField
 from wtforms.fields.simple import PasswordField
-
+from flask.helpers import flash, url_for
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "clavesecreta"
 
@@ -15,11 +16,25 @@ def index():
 
 @app.route("/<string:seccion>") 
 def secciones(seccion): 
+    nombre = None
     if seccion == "hombre":
         nombre = "HOMBRES"
-    elif seccion == "hombre":
-        nombre = "HOMBRES"
-    return render_template("secciones.html", nombre = nombre )
+    elif seccion == "mujer":
+        nombre = "MUJERES"
+    elif seccion == "niño":
+        nombre = "NIÑOS"
+    elif seccion == "descuento":
+        nombre = "DESCUENTOS"
+
+    if nombre == None:
+        return redirect(url_for('error'))
+    else:
+        return render_template("secciones.html", nombre = nombre )
+
+
+@app.route('/error') 
+def error(): 
+   return render_template("error.html")
 
 if __name__ == "__main__":  # Makes sure this is the main process
     app.run(  # Starts the site
